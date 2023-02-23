37f88302d375667b227a04fda6b6736ce845f0c8
Levi.Ding <levi.ding@d1m.cn>
2023-02-23 18:01:25 +0800

    Flux producing Demo

--- src/main/java/com/cn/demo/producing/Demo5.java
+++ src/main/java/com/cn/demo/producing/Demo5.java
@@ -1,10 +1,47 @@
 package com.cn.demo.producing;
 
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.Disposable;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+
 /**
- * @Description todo
+ * @Description Flux clear
+ * onDispose 终止时清理 （出错或者取消时触发） 终止操作
+ * onCancel 取消信号
+ * 执行顺序 关键代码
+ *
+ *
+ * {@link reactor.core.publisher.FluxCreate.BaseSink#disposeResource(boolean)} 中
+ *                 if (d != null && d != TERMINATED && d != CANCELLED) {
+ * 					if (isCancel && d instanceof SinkDisposable) {
+ * 						((SinkDisposable) d).cancel();
+ *                  }
+ * 					d.dispose();
+ * 				}
+ *
+ * 取消时  cancel ->  dispose
+ * 报错时  dispose
+ * 完成时  dispose
  * @Author: Levi.Ding
  * @Date: 2023/2/23 16:56
  * @Version V1.0
  */
+@Slf4j
 public class Demo5 {
+
+
+    public static void main(String[] args) {
+        Disposable first = Flux.create(s -> {
+            s.next("First");
+            s.onCancel(() -> log.info("Flux Cancel!"));
+            s.onDispose(() -> log.info("Flux Dispose!"));
+//            s.complete();
+        }).delayElements(Duration.ofSeconds(10)).subscribe(i -> log.info("Flux Subscribe i : {}", i));
+
+        first.dispose();
+
+
+    }
 }
