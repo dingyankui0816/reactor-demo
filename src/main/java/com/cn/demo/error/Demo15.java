d1c744e56d806c13f3513e3a5d2250b598492f0f
Levi.Ding <levi.ding@d1m.cn>
2023-03-24 17:23:47 +0800

    Flux error Demo

--- src/main/java/com/cn/demo/error/Demo15.java
+++ src/main/java/com/cn/demo/error/Demo15.java
@@ -1,12 +1,24 @@
 package com.cn.demo.error;
 
 import lombok.extern.slf4j.Slf4j;
+import reactor.core.Disposable;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Consumer;
+import java.util.function.Function;
 
 /**
  * @Description
  * Flux error  VS   try-catch
+ *
+ *doc <a href="https://projectreactor.io/docs/core/release/reference/index.html#_error_handling_operators">
+ *
  *
  * @Author: Levi.Ding
  * @Date: 2023/3/17 16:00
@@ -15,14 +27,16 @@ import reactor.core.publisher.Mono;
 @Slf4j
 public class Demo15 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException {
 //        onErrorVsTryCatch();
 //        onErrorReturnVsTryCatch();
 //        onErrorCompleteVsTryCatch();
 //        onErrorResumeVsOnErrorReturn();
 //        onErrorResumeVsTryCatch();
 //        onErrorMapVsTryCatch();
-        doOnErrorVsTryCatch();
+//        doOnErrorVsTryCatch();
+//        doFinallyVsTryCatch(3);
+        using();
     }
 
 
@@ -328,5 +342,89 @@ public class Demo15 {
             log.info("由于异常，进行XXXX操作");
             throw e;
         }
+    }
+
+
+    public static  void doFinallyVsTryCatch(int type) throws IOException, InterruptedException {
+        log.info("-------------------{}---------------------", "doFinally");
+        Flux<Integer> range = Flux.range(1, 10);
+        if (type == 2){
+            range = range.subscribeOn(Schedulers.newSingle("Levi"));
+
+        }
+        Disposable di = range.flatMap(i -> {
+            int j = i * 2;
+            if (j == 10) {
+                if (type == 1) {
+                    throw new RuntimeException("FlatMap Exception");
+                } else if (type == 2) {
+                    try {
+                        TimeUnit.SECONDS.sleep(10);
+                        synchronized (Demo15.class){
+                            Demo15.class.notifyAll();
+                        }
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                } else {
+
+                }
+            }
+            return Flux.just(j);
+        }).doFinally(t -> {
+            log.info("finally info,t:{}", t);
+        }).subscribe(i -> log.info("i:{}", i), e -> log.info("e:", e), () -> log.info("complete!"));
+        if (type == 2){
+            synchronized (Demo15.class){
+                Demo15.class.wait();
+                di.dispose();
+            }
+        }
+
+        log.info("-------------------{}---------------------", "try-catch");
+
+        try {
+            for (int i = 1; i <= 10; i++) {
+                if (i * 2 == 10) {
+                    if (type == 1){
+
+                    }else if (type == 2 ){
+                        throw new RuntimeException("Test tryCatchOnError");
+
+                    }
+                }
+                log.info("i:{}", i * 2);
+
+            }
+            log.info("complete!");
+        } catch (Exception e) {
+            throw e;
+        }finally {
+            log.info("finally info");
+        }
+    }
+
+
+    /**
+     * @Description: {@link Flux#using(Callable, Function, Consumer)}
+     * {@link Callable} ： 初始化方法  {@link reactor.core.publisher.FluxUsing#subscribe(reactor.core.CoreSubscriber)} 中 resource = resourceSupplier.call();
+     * {@link Function} ： 基于初始化数据 生成 流逻辑 {@link reactor.core.publisher.FluxUsing#subscribe(reactor.core.CoreSubscriber)} 中
+     * p = Objects.requireNonNull(sourceFactory.apply(resource),
+     * 					"The sourceFactory returned a null value");
+     *
+     * {@link Consumer}	： 可以将初始化数据进行回收等操作 {@link reactor.core.publisher.FluxUsing.UsingSubscriber#onComplete()} / {@link reactor.core.publisher.FluxUsing.UsingSubscriber#onError(Throwable)} / {@link reactor.core.publisher.FluxUsing.UsingSubscriber#cancel()} 中 resourceCleanup.accept(resource);
+     *
+     * @author Levi.Ding
+     * @date 2023/3/24 16:56
+     * @return : void
+     */
+    public static void using(){
+
+        log.info("-------------------{}---------------------", "using");
+        List<Integer> list = Flux.using(() -> 10, i -> Flux.range(1, 10).map(j -> j * i), t -> log.info("finally info,t:{}", t)).collectList().block();
+
+        list.forEach(i -> {log.info(" i : {}",i);});
+        
+        
     }
 }
