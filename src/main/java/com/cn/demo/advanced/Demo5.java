9df035eee26a336e8457645277b909c9c4d41c7d
Levi.Ding <levi.ding@d1m.cn>
2023-04-24 13:23:05 +0800

    Flux advanced Demo

--- src/main/java/com/cn/demo/advanced/Demo5.java
+++ src/main/java/com/cn/demo/advanced/Demo5.java
@@ -10,6 +10,9 @@ import java.util.concurrent.TimeUnit;
 
 /**
  * @Description 并行化 Flux
+ *
+ *      *  doc <a href="https://projectreactor.io/docs/core/release/reference/index.html#advanced-parallelizing-parralelflux">
+ *
  * @Author: Levi.Ding
  * @Date: 2023/4/11 14:50
  * @Version V1.0
@@ -18,26 +21,53 @@ import java.util.concurrent.TimeUnit;
 public class Demo5 {
 
 
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+//        testParallel();
+        testParallelThenMerge();
+    }
+
     /**
      * @Description: 分片并行订阅 ,每一个订阅绑定一个 {@link reactor.core.scheduler.Scheduler#createWorker()}
      *
      *  {@link reactor.core.publisher.ParallelRunOn#subscribe(reactor.core.CoreSubscriber[])} ->
      *  {@link reactor.core.publisher.ParallelSource#subscribe(reactor.core.CoreSubscriber[])}
      *
+     *  多订阅者基于轮训的负载均衡策略处理
+     *  {@link reactor.core.publisher.ParallelSource.ParallelSourceMain#drain()}
      *
-     *  doc <a href="https://projectreactor.io/docs/core/release/reference/index.html#advanced-parallelizing-parralelflux">
+     *
      *
      *
      * @author Levi.Ding
      * @date 2023/4/11 16:05
-     * @param args :
+     * @param
      * @return : void
      */
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void testParallel() throws InterruptedException, IOException {
         Flux.range(1,10).parallel(2).runOn(Schedulers.boundedElastic()).subscribe(i -> log.info("ThreadName : {},i : {}",Thread.currentThread().getName(),i));
         TimeUnit.SECONDS.sleep(5);
         Flux.range(1,10).parallel(2).runOn(Schedulers.boundedElastic()).subscribe(i -> log.info("ThreadName : {},i : {}",Thread.currentThread().getName(),i));
 
         System.in.read();
     }
+
+    /**
+     * @Description: sequential 使用的线程是第一个 获取到 wip 锁的 线程
+     * {@link reactor.core.publisher.ParallelMergeSequential.MergeSequentialMain#onNext(reactor.core.publisher.ParallelMergeSequential.MergeSequentialInner, java.lang.Object)}
+     * @author Levi.Ding
+     * @date 2023/4/23 11:14
+     * @return : void
+     */
+    public static void testParallelThenMerge() throws InterruptedException, IOException {
+        Flux.range(1,10).parallel(2).runOn(Schedulers.boundedElastic())
+                .map(i -> i *2 ).sequential()
+                .subscribe(i -> log.info("ThreadName : {},i : {}",Thread.currentThread().getName(),i));
+        System.in.read();
+    }
+
+
+
+
+
 }
---
+++ src/main/java/com/cn/demo/advanced/Demo6.java
@@ -0,0 +1,19 @@
+package com.cn.demo.advanced;
+
+/**
+ * @Description Flux.context
+ * @Author: Levi.Ding
+ * @Date: 2023/4/23 16:20
+ * @Version V1.0
+ */
+public class Demo6 {
+
+    public static void main(String[] args) {
+
+    }
+
+
+    public static void testContext1(){
+
+    }
+}
